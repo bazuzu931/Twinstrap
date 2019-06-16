492950bf4a553cce503f21b6adb1ad9c60337cc9
bazuzu931 <bazuzu931@yandex.ru>
2019-06-16 15:25:39 +0400

    coomponents page

--- src/pages/Compon.vue
+++ src/pages/Compon.vue
@@ -2,79 +2,89 @@
   <Layout>
      <div class="container mx-auto py-8 sm:py-10 flex ">
      	<div class="nav-list  w-1/5 p-8 ">
-			<ul class=" h-180 overflow-y-scroll  ">
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-				<li>dsds</li>
-			</ul>
+
+
+			  <!--  Panel 1  -->
+			  <div class="text-gray-600">
+			    <input type="checkbox" name="panel" id="panel-1" class="hidden">
+			    <label for="panel-1" class="relative block  p-2  border-b border-grey hover:text-green-500">Labels</label>
+			    <div class="accordion__content overflow-hidden ">
+			      <ul class="accordion__body p-4" id="panel1">
+					<li class=""><a href="#" class="block p-3 hover:text-black">Label 1</a></li>
+					<li class=""><a href="#" class="block p-3 hover:text-black">Label 2</a></li>
+					<li class=""><a href="#" class="block p-3 hover:text-black">Label 3</a></li>
+			      </ul>
+			    </div>
+			  </div>
+
+
+			  <!-- Panel 2 -->
+				<div class="text-gray-600">
+				    <input type="checkbox" name="panel" id="panel-2" class="hidden">
+				    <label for="panel-2" class="relative block  p-2  border-b border-grey hover:text-green-500">Buttons</label>
+				    <div class="accordion__content overflow-hidden ">
+				      <ul class="accordion__body p-4" id="panel1">
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 1</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 2</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 3</a></li>
+				      </ul>
+				    </div>
+				  </div>
+
+				  <div class="text-gray-600">
+				    <input type="checkbox" name="panel" id="panel-3" class="hidden">
+				    <label for="panel-3" class="relative block  p-2  border-b border-grey hover:text-green-500">Buttons</label>
+				    <div class="accordion__content overflow-hidden ">
+				      <ul class="accordion__body p-4" id="panel1">
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 1</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 2</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 3</a></li>
+				      </ul>
+				    </div>
+				  </div>
+
+				  <div class="text-gray-600">
+				    <input type="checkbox" name="panel" id="panel-4" class="hidden">
+				    <label for="panel-4" class="relative block  p-2  border-b border-grey hover:text-green-500">Buttons</label>
+				    <div class="accordion__content overflow-hidden ">
+				      <ul class="accordion__body p-4" id="panel1">
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 1</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 2</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 3</a></li>
+				      </ul>
+				    </div>
+				  </div>
+
+				  <div class="text-gray-600">
+				    <input type="checkbox" name="panel" id="panel-5" class="hidden">
+				    <label for="panel-5" class="relative block  p-2  border-b border-grey hover:text-green-500">Buttons</label>
+				    <div class="accordion__content overflow-hidden ">
+				      <ul class="accordion__body p-4" id="panel1">
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 1</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 2</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 3</a></li>
+				      </ul>
+				    </div>
+				  </div>
+
+				  <div class="text-gray-600">
+				    <input type="checkbox" name="panel" id="panel-6" class="hidden">
+				    <label for="panel-6" class="relative block  p-2  border-b border-grey  hover:text-green-500">Buttons</label>
+				    <div class="accordion__content overflow-hidden ">
+				      <ul class="accordion__body p-4" id="panel1">
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 1</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 2</a></li>
+						<li class=""><a href="#" class="block p-3 hover:text-black">Button 3</a></li>
+				      </ul>
+				    </div>
+				  </div>
+
+
      	</div>
 
 
 
-     	<div class="w-full md:w-3/5 mx-auto p-8">
-         <div class="shadow-md">
-            <div class="tab w-full overflow-hidden border-t">
-               <input class="absolute opacity-0" id="tab-single-one" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p class="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-               </div>
-            </div>
-            <div class="tab w-full overflow-hidden border-t">
-               <input class="absolute opacity-0" id="tab-single-two" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer" for="tab-single-two">Label Two</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p class="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-               </div>
-            </div>
-            <div class="tab w-full overflow-hidden border-t">
-               <input class="absolute opacity-0" id="tab-single-three" type="radio" name="tabs">
-               <label class="block p-5 leading-normal cursor-pointer" for="tab-single-three">Label Three</label>
-               <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p class="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-               </div>
-            </div>
-         </div>
-      </div>
+
 
      	<div class="main  w-4/5 p-8">
      		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint nam laudantium quis sapiente odio atque voluptas cupiditate quidem, a amet, similique maiores aut iste, sunt sed quae minima optio suscipit dolores cum? Excepturi deleniti officiis doloribus mollitia eaque repellendus dolore iure voluptate aut magni porro aliquid earum eius tenetur non corporis laboriosam, repellat quia culpa ullam. Minus sed iure perferendis porro, labore odit ipsum libero esse veritatis! Corrupti repellendus facilis commodi consectetur ad quia provident, adipisci iusto minus distinctio aspernatur quidem sed possimus itaque eligendi, quam sint! Cumque, deserunt, expedita. Laboriosam, quisquam. Neque a, deserunt sequi officia at incidunt mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus nihil saepe, nobis voluptatum, veritatis, perspiciatis officiis eligendi natus a sunt ea debitis excepturi ratione incidunt. Sit nisi, dolorum quidem harum doloribus cumque, quos voluptatem saepe, placeat cupiditate quia, obcaecati facilis autem et. Ex ipsum veniam, doloremque dolore impedit ut, quia similique minus temporibus reiciendis enim, non deleniti, exercitationem tenetur ullam nemo. Facere placeat accusantium, cumque quisquam, impedit libero fuga tempora est eveniet, molestiae ut atque! Omnis quibusdam et esse, excepturi, non, perferendis, nemo consectetur itaque vero error laudantium blanditiis similique rem vitae possimus consequatur magni nam ducimus quas est dicta nostrum maiores quaerat culpa. Aspernatur iure error dolores nemo provident inventore, explicabo molestias natus ratione dolor alias vitae quo officiis atque corporis quibusdam adipisci? Doloribus soluta quis fugiat deserunt quam labore perspiciatis perferendis libero officia! Molestias hic veniam inventore distinctio voluptatum eius, aut quis similique provident. Recusandae ad illo neque repudiandae, fugiat nostrum explicabo, cupiditate culpa, minima expedita eaque beatae vitae deserunt accusantium distinctio quos ipsa quod praesentium autem unde veniam pariatur. Tempore quod reiciendis nam doloribus, optio sint qui enim. Culpa minima necessitatibus ducimus animi expedita. Sapiente unde, eum libero distinctio consequuntur ducimus nisi numquam repudiandae dolores possimus quas animi quia quis ipsam, pariatur! Aspernatur architecto laborum earum nam quos est modi, sit asperiores, cupiditate, molestiae eligendi nemo praesentium ipsam? Nesciunt, iusto. Odio esse, consectetur quibusdam dolor. Deleniti dolor excepturi, quam, illum sapiente earum. Cupiditate ratione aut itaque alias labore vero maxime harum laudantium, necessitatibus earum voluptates assumenda repudiandae quos, iusto suscipit minus magni facilis deleniti. Velit officiis veritatis ipsam, necessitatibus hic vero sunt id reiciendis? Odit facere ipsum fuga aut saepe obcaecati magnam voluptatum quo quae. Iure quidem, veniam fugiat recusandae molestias! Blanditiis nisi, ratione consequuntur, doloremque commodi ullam culpa deleniti enim obcaecati nulla error natus sint ipsam, sapiente quis labore voluptatem. Quo accusamus laboriosam dolor, assumenda aliquam, labore sunt dolorem culpa eum unde facere nemo, porro! Quod, totam. Ea explicabo optio soluta ullam accusamus rem libero, accusantium eos aliquam quas molestias nostrum inventore amet officia praesentium, molestiae quos id quis perferendis nesciunt vel tempora velit rerum. Officia consequatur labore, deserunt incidunt beatae illo est reprehenderit excepturi temporibus blanditiis doloremque, autem corporis dignissimos velit laborum dolorum. Atque id aliquam ipsa perspiciatis hic suscipit minima alias ab ipsum autem laboriosam beatae officiis vel temporibus labore et quidem sed repudiandae officia facere, ullam natus. Libero dolore officia ducimus dignissimos ipsum eius eos soluta sequi! Assumenda sit libero in, vitae commodi dicta laborum ex delectus voluptates, beatae fuga, ea. Nihil in nisi quibusdam cumque minima quisquam atque autem ex vel accusantium? Expedita atque voluptatem possimus eligendi minima, quasi vel dignissimos cupiditate laborum qui neque minus officiis culpa nisi molestias harum aut quam cum tempora reiciendis pariatur aspernatur eaque reprehenderit quos. Debitis qui maxime iure quos ullam nemo itaque sunt pariatur totam voluptatum eveniet voluptates animi, architecto facere neque corporis, dolore ipsum, impedit quidem aliquid ipsam. Autem maiores facere eius, id recusandae laudantium sint animi magni magnam mollitia ea doloribus tempore nulla nesciunt adipisci, quisquam ad dignissimos iste, repellendus doloremque similique odio, expedita sit! Iste ullam magni nobis mollitia commodi asperiores. Mollitia in repudiandae voluptatum, maxime esse at veritatis, velit non! Corporis odit porro provident, recusandae eveniet consequuntur tempore ab velit soluta voluptates, eaque, nobis reiciendis neque, aspernatur ad? Delectus provident soluta blanditiis. Eos alias, consectetur enim harum dignissimos, repellendus, debitis dolor velit quasi aliquid officia perspiciatis, delectus in itaque. Ex aperiam cupiditate nesciunt animi quas reprehenderit nulla, reiciendis nostrum dolorum voluptas, aut ipsam, deleniti, vitae exercitationem dolor hic dicta neque atque tempora enim! Sit commodi mollitia officiis neque facilis alias magni dolore excepturi quas quibusdam porro ullam sed molestias, ab, voluptates! Architecto quidem et quae, ex minima error corporis amet ipsa eos inventore cumque rem ea, mollitia porro, labore fugiat delectus obcaecati quod facere. Cumque temporibus laborum, nisi. Sit iusto officiis quam distinctio repudiandae reiciendis earum incidunt quasi voluptas animi vitae doloremque sapiente placeat consequatur eaque, tempora ea optio. Deleniti, eius! Laborum similique aut repellat quasi dolores magnam sed unde obcaecati sint, reprehenderit corporis possimus iusto aspernatur, voluptatum officiis adipisci incidunt necessitatibus, exercitationem mollitia, aliquid expedita rem dolorem quis. Rem quaerat, dolorem et inventore dolor ipsum, suscipit ipsam tenetur deserunt soluta. Sint delectus, a doloribus reprehenderit accusamus officia voluptatibus? Maxime praesentium vitae animi, distinctio dolores ullam, blanditiis! Delectus expedita ex, dolorum deserunt, reprehenderit hic dolores distinctio omnis sunt mollitia. Ipsa, corrupti quaerat quae voluptatem possimus commodi ut, voluptates alias quam itaque in consequuntur recusandae saepe veniam laborum velit laudantium labore! Voluptates dignissimos dolores libero illo dolore tenetur sed, sit odit pariatur? Corporis natus quibusdam dolores recusandae ducimus tempore. Aperiam sequi, inventore voluptatibus! Quo fuga magni cum dolorum porro unde corporis blanditiis sequi laboriosam, nesciunt molestias, totam saepe dicta doloribus necessitatibus ipsum eius fugiat cupiditate nulla recusandae a asperiores qui suscipit! Repudiandae rerum pariatur, veniam porro eveniet amet odio aperiam, perspiciatis mollitia, dolore inventore minus, ea officiis! Deserunt mollitia voluptatibus quis debitis minus aut assumenda facere atque, vitae, qui, odio rerum amet. Hic rem, nesciunt voluptatibus, maxime facere nisi a reprehenderit atque explicabo quis sequi perferendis consectetur possimus laboriosam ex libero magnam ipsam et temporibus totam quos provident. Aut, quis, reiciendis ad adipisci tempore, a quod non molestias voluptatum ratione vel. Alias nobis iure incidunt dolore officia nesciunt facere, cumque maiores, illum repudiandae, nam error, quidem quisquam doloribus architecto consequatur iusto pariatur quo aspernatur vero eos eveniet in. Doloremque cupiditate fugit voluptate consectetur laboriosam nesciunt adipisci ab mollitia error quibusdam in quos quaerat, voluptates pariatur illo placeat. In nisi deserunt doloremque. Delectus eius rem in, nesciunt vitae? Aperiam tempore explicabo ea facere alias aliquid sint aliquam consequuntur quas a, quia, odio eum tempora rem, unde incidunt laboriosam consequatur laborum. Provident iusto nisi repellat repudiandae iste, illo voluptas, quam necessitatibus iure, non maiores illum doloribus nemo officia molestias laboriosam! Ducimus debitis dicta cupiditate qui cum ea, rem maxime magnam adipisci corporis! Recusandae eum optio dolore laborum nemo, accusamus sed ipsam, voluptatum consequuntur sit veritatis, excepturi ex assumenda nostrum ducimus! Nulla sint nam necessitatibus!
@@ -93,66 +103,39 @@ export default {
 
 
 <style>
-         /* Tab content - closed */
-         .tab-content {
-         max-height: 0;
-         -webkit-transition: max-height .35s;
-         -o-transition: max-height .35s;
-         transition: max-height .35s;
-         }
-         /* :checked - resize to full height */
-         .tab input:checked ~ .tab-content {
-         max-height: 100vh;
-         }
-         /* Label formatting when open */
-         .tab input:checked + label{
-         /*@apply text-xl p-5 border-l-2 border-indigo-500 bg-gray-100 text-indigo*/
-         font-size: 1.25rem; /*.text-xl*/
-         padding: 1.25rem; /*.p-5*/
-         border-left-width: 2px; /*.border-l-2*/
-         border-color: #6574cd; /*.border-indigo*/
-         background-color: #f8fafc; /*.bg-gray-100 */
-         color: #6574cd; /*.text-indigo*/
-         }
-         /* Icon */
-         .tab label::after {
-         float:right;
-         right: 0;
-         top: 0;
-         display: block;
-         width: 1.5em;
-         height: 1.5em;
-         line-height: 1.5;
-         font-size: 1.25rem;
-         text-align: center;
-         -webkit-transition: all .35s;
-         -o-transition: all .35s;
-         transition: all .35s;
-         }
-         /* Icon formatting - closed */
-         .tab input[type=checkbox] + label::after {
-         content: "+";
-         font-weight:bold; /*.font-bold*/
-         border-width: 1px; /*.border*/
-         border-radius: 9999px; /*.rounded-full */
-         border-color: #b8c2cc; /*.border-grey*/
-         }
-         .tab input[type=radio] + label::after {
-         content: "\25BE";
-         font-weight:bold; /*.font-bold*/
-         border-width: 1px; /*.border*/
-         border-radius: 9999px; /*.rounded-full */
-         border-color: #b8c2cc; /*.border-grey*/
-         }
-         /* Icon formatting - open */
-         .tab input[type=checkbox]:checked + label::after {
-         transform: rotate(315deg);
-         background-color: #6574cd; /*.bg-indigo*/
-         color: #f8fafc; /*.text-grey-lightest*/
-         }
-         .tab input[type=radio]:checked + label::after {
-         transform: rotateX(180deg);
-         background-color: #6574cd; /*.bg-indigo*/
-         color: #f8fafc; /*.text-grey-lightest*/
-         }
+.accordion__body>li>a {
+	-webkit-transition:  .2s;
+	   -moz-transition:  .2s;
+	    -ms-transition:  .2s;
+	     -o-transition:  .2s;
+	        transition:  .2s;
+}
+.accordion__body>li>a:hover {
+	padding-left: 15px;
+	border-left: 2px solid #48bb78;
+
+}
+label {
+	cursor: pointer;
+}
+label:after {
+  content: '+';
+  position: absolute;
+  right: 1em;
+  color: #fff;
+}
+
+input:checked + label:after {
+  content: '-';
+  line-height: .8em;
+}
+
+.accordion__content{
+  max-height: 0em;
+  transition: all 0.4s cubic-bezier(0.865, 0.14, 0.095, 0.87);
+}
+input[name='panel']:checked ~ .accordion__content {
+  /* Get this as close to what height you expect */
+  max-height: 50em;
+}
       </style>
