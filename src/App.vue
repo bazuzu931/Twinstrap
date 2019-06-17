a6da89a48b0cf4095b4076a7afcc6e524d1562fb
bazuzu931 <bazuzu931@yandex.ru>
2019-06-18 00:40:30 +0400

    init nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
-    <p class="bg-blue-500">ello</p>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <main class="flex flex-col bg-gray-500 h-screen ">
+
+      <nav class="flex  border-b border-green-500 h-10 ">
+        <div class="w-4/6 flex justify-between border border-blue-600 mx-auto">
+          <div class="logo">logo</div>
+          <div class="up-menu flex border border-red-500">
+            <div class="components">Components</div>
+            <div class="templates">Templates</div>
+          </div>
+        </div>
+      </nav>
+
+      <div class="bg-green-200 mt-10 h-screen flex">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos nesciunt odit libero, accusamus animi minima eius dolores totam vitae, incidunt ducimus dolor numquam voluptatibus, voluptatem qui, consequatur itaque! Cumque veritatis inventore ullam obcaecati consequuntur explicabo alias! Ab totam laborum voluptas perferendis provident, culpa blanditiis ducimus, aliquam esse aperiam aut perspiciatis ipsam, eum molestias fugiat aspernatur. Dolor unde itaque, fuga quaerat eos quasi ex inventore, facere fugiat autem libero dolorum. Explicabo adipisci in, aspernatur. Saepe laborum delectus, explicabo a asperiores libero ullam beatae labore porro, sint magnam illo consequuntur officia ex quis sapiente deleniti facere possimus nostrum? Incidunt sapiente cupiditate, corporis!</div>
+
+    </main>
   </div>
 </template>
 
@@ -25,7 +37,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 </style>
