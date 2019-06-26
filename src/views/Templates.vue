45394ab240400d47420adaf01833f9d56f36383a
bazuzu931 <bazuzu931@yandex.ru>
2019-06-26 13:59:44 +0400

    init admin panel

--- src/views/Templates.vue
+++ src/views/Templates.vue
@@ -1,36 +1,42 @@
 <template>
 	<!-- Main content -->
-	<div class="flex w-4/6 justify-between mx-auto ">
+	<div class="flex flex-col w-3/6 justify-between mx-auto border border-black">
 
 
-	    <nav class="left-navbar w-1/5 flex flex-col  ">
-	      	<div class="">
-	      		<ul class="h-screen fixed overflow-y-auto pr-12 pb-16">
 
-				    <!--  Panel 1 [Label]  -->
-		            <li class="" :key="1">
-		                <router-link to="/templates/admin" class="flex block text-gray-600 hover:text-black p-2" @click.native="contentVisible === 1 ? contentVisible = false : contentVisible = 1">Admin</router-link>
-		                <!-- <div class="" v-if='contentVisible === 1'>
-			                <li class=""><a href="#label-1" class="flex block text-gray-600  hover:text-black pl-7 p-3">Label 1</a></li>
-			                <li class=""><a href="#label-2" class="flex block text-gray-600  hover:text-black pl-7 p-3">Label 2</a></li>
-			                <li class=""><a href="#label-3" class="flex block text-gray-600  hover:text-black pl-7 p-3">Label 3</a></li>
-		                </div> -->
-		            </li>
+		<div class="flex flex-grow border border-green-700">
+			<div class="flex  border border-red-500 p-2 m-4">
+				<a href="#" class="max-w-sm rounded overflow-hidden shadow-lg ">
+					  <img class="w-full" src="./../assets/templates/admin/admin-1.png" alt="Sunset in the mountains">
+					  <div class="px-6 py-4 bg-teal-600">
+					    <div class="font-bold text-xl mb-2 text-white">Admin</div>
+					    <p class="text-white text-base">
+					      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+					    </p>
+					  </div>
+				</a>
+			</div>
+			<div class="flex flex-grow border border-red-500 p-2 m-4">
+				sdsd
+			</div>
+			<div class="flex flex-grow border border-red-500 p-2 m-4">
+				sdsd
+			</div>
+		</div>
 
-		            <!--  Panel 2 [Button]  -->
-		            <li class="" :key="2">
-		                <router-link to="/templates/simple-blog-page" class="flex block text-gray-600 hover:text-black p-2" @click.native="contentVisible === 2 ? contentVisible = false : contentVisible = 2">Simple Blog Page</router-link>
-		            </li>
-		        </ul>
-	      	</div>
-	    </nav>
-	    <!-- End of Left nav -->
+		<div class="flex flex-grow border border-green-700">
+			<div class="flex flex-grow border border-red-500 p-2 m-4">
+				sdsd
+			</div>
+			<div class="flex flex-grow border border-red-500 p-2 m-4">
+				sdsd
+			</div>
+			<div class="flex flex-grow border border-red-500 p-2 m-4">
+				sdsd
+			</div>
+		</div>
 
 
-	    <div class="content w-4/5 p-4">
-	        <router-view/>
-	    </div>
-	    <!-- End of content -->
 
 	</div>
 	<!-- End  of Main content -->
